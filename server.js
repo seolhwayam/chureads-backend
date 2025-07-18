07fc1c7020f5eecf4a3bc75188858307c76ec6ce
seolhwayam <susung462@gmail.com>
2025-07-18 11:27:45 +0900

    commit

--- server.js
+++ server.js
@@ -1,32 +1,30 @@
 import express from "express";
 import dotenv from "dotenv";
 import { testTagGenerate } from "./services/tagService.js";
-import posts, {init} from "./routes/posts.js";
+import postRouter, { init } from "./routes/posts.js";
 import { connectDB } from "./database/db.js";
 import cors from "cors";
 
-//환경변수 로드
-//전역으로 로드해서 모든 node, js모듈 내에서 접근가능
-
+// 환경변수 로드
+// 전역으로 로드해서 모든 node.js모듈 내에서 접근가능
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT;
 
+// CORS 설정 - 순서 중요⭐️
+app.use(cors()); // 모든 도메인 허용
 
-//CORS 설정
-app.use(cors()); //모든 도메인 허용
-
-//json파싱 설정
+// Json 파싱 설정
 app.use(express.json());
-app.use(express.urlencoded({extended:true}));
+app.use(express.urlencoded({ extended: true }));
 
-//라우터 미들웨어 등록
-app.use("/posts",posts);
-//app.use("user",userRouter);
+// 라우터 미들웨어 등록
+app.use("/posts", postRouter);
 
-app.listen(PORT, async() => {
-    console.log("Server Start!!");
-    const db = await connectDB();
-    init(db);
-})
+app.listen(PORT, async () => {
+  console.log("Server at..", PORT);
+  // testTagGenerate();
+  const db = await connectDB();
+  init(db);
+});
